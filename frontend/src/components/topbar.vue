9cf0831b436ae81c46e1e3b5ab4c83193504b538
andero-magi <andero.magi.420+schoolgit@gmail.com>
2025-01-06 22:05:57 +0200

    New post link to the topbar

--- frontend/src/components/topbar.vue
+++ frontend/src/components/topbar.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-darker border-bottom border-light">
     <div class="container-fluid">
@@ -8,7 +13,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+          <li>
+            <RouterLink class="nav-link" to="/newpost">New Post</RouterLink>
           </li>
         </ul>
       </div>
