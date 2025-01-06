f704f55b0acf0b077b280a0a520b8408e2fb3e41
Opilane TTHK <opilane@tthk.ee>
2025-01-06 15:44:46 +0200

    Create consts.js

---
+++ frontend/src/consts.js
@@ -0,0 +1,6 @@
+export const API_URL = "http://localhost:8080"
+
+export const SEARCH_ADD_EVENT = "tagadd"
+export const SEARCH_REMOVE_EVENT = "tagrem"
+export const SEARCH_EXCLUDE_EVENT = "tagexl"
+export const SEARCH_SET_EVENT = "tagset"
