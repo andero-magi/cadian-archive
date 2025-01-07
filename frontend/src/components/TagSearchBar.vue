909a265a5f4598f13b5dc72c294327af920ca4ac
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2025-01-07 14:09:06 +0200

    Create TagSearchBar.vue

---
+++ frontend/src/components/TagSearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="text" @change="onInputTyped">
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emits = defineEmits(["change"])
+const tags = ref<string[]>([])
+
+function onInputTyped(event: Event): void {
+  
+}
+</script>
