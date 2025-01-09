96ee1fd882cbfab6ec80455f0aa17abcdafd9417
andero-magi <andero.magi.420+schoolgit@gmail.com>
2025-01-10 01:21:03 +0200

    Update PostList.vue

--- frontend/src/views/PostList.vue
+++ frontend/src/views/PostList.vue
@@ -1,21 +1,28 @@
 <template> 
   <div class="d-block w-100 my-5">
-    <div class="flex-column p-4 bg-darker mx-auto" style="width: 90%; height: 80vh;">
-      <div v-for="post in foundPosts">
-        <RouterLink :to="{name: 'post', params: {id: post.id}}">{{ post.id }}</RouterLink>
-      </div>
+    <div class="d-flex align-items-start flex-wrap flex-row gap-3 p-4 bg-darker mx-auto" style="width: 90%; height: 80vh;">
+      <RouterLink :to="{name: 'post', params: {id: post.id}, query: {...route.query}}" class="card postcard" v-for="post in foundPosts">
+        <template v-for="c in post.content">
+          <h5 v-if="c.type == 'title'" class="card-title">{{ c.data }}</h5>
+          <h6 v-if="c.type == 'header'" class="card-subtitle">{{ c.data }}</h6>
+          <img v-if="c.type == 'imageref'" class="card-img" :src="`${API_URL}/images/${c.data}`">
+          <img v-if="c.type == 'imagedata'" class="card-img" :src="c.data">
+          <div v-if="c.type == 'section'" class="card-body card-text">{{ c.data }}</div>
+        </template>
+      </RouterLink>
     </div>
   </div>
 </template>
 
-<script setup>
-import { onBeforeMount, ref, watch } from 'vue';
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { API_URL } from '@/consts';
 import router from '@/router';
+import { Content, Post } from '@/post';
 
 let route = useRoute()
-let foundPosts = ref([]);
+let foundPosts = ref<Post[]>([]);
 
 onBeforeMount(reloadShit);
 router.afterEach(async (to, from, fail) => await reloadShit())
@@ -27,18 +34,47 @@ async function reloadShit() {
 
 async function doPostSearch(queryParam) {
   let queryUrl = `${API_URL}/posts?search=${encodeURIComponent(queryParam)}`
-  console.log(`queryUrl=${queryUrl}`) 
 
   let response = await fetch(queryUrl)
   if (!response.ok) {
     return
   }
 
-  let json = await response.json()
-  foundPosts.value = json
+  let json: Post[] = await response.json()
 
-  console.log(json)
-  console.log(foundPosts.value)
+  const MAX_TEXT_SIZE = 50
+  const MAX_CONTENT_SIZE = 3
+
+  for (let p of json) {
+    let content: Content[] = p.content
+
+    if (content.length > MAX_CONTENT_SIZE) {
+      p.content = content = content.slice(0, MAX_CONTENT_SIZE)
+    }
+
+    for (let c of content) {
+      if (c.type.startsWith("image")) {
+        continue
+      }
+
+      let data = c.data 
+      if (data.length > MAX_TEXT_SIZE) {
+        data = data.substring(0, MAX_TEXT_SIZE) + "..."
+      }
+
+      c.data = data
+    }
+  }
+
+  foundPosts.value = json
 }
 
 </script>
+
+<style>
+.postcard {
+  width: 250px;
+  padding: 10px;
+  text-decoration: none;
+}
+</style>
