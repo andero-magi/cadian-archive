30192c1cdc1f899fb9e0d8b577bd9cc520901603
andero-magi <andero.magi.420+schoolgit@gmail.com>
2025-01-10 01:17:15 +0200

    Update utils.ts

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,6 +1,7 @@
 import { API_URL } from "./consts";
+import { Post } from "./post";
 
-export async function getPost(id: string): Promise<any> {
+export async function getPost(id: string): Promise<Post> {
   let fetchUrl = `${API_URL}/posts/${id}`
   let response = await fetch(fetchUrl)
 
@@ -9,9 +10,5 @@ export async function getPost(id: string): Promise<any> {
   }
   
   let json = await response.json()
-
-  json.modified_date = new Date(json.modified_date)
-  json.upload_date = new Date(json.upload_date)
-
   return json
 }
